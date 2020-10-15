2e19b2f508d70a5e33412c0cf65a53e608386d95
Jianhang Chen <cjhastro@gmail.com>
2020-10-15 16:38:28 +0200

    add band surpport

--- code/gen_image.py
+++ code/gen_image.py
@@ -1,6 +1,15 @@
-def gen_image(obj):
+def gen_image(obj, band=None):
     for obs in os.listdir(obj):
-        make_cont_img(vis=obs, dirty_image=True)
+        if band is not None:
+            band_match = re.compile('_(?P<band>B\d{1,2})')
+            if band_match.search(obs):
+                obs_band = band_match.search(obs).groupdict()['band']
+                if obs_band != band:
+                    continue
+        try:
+            make_cont_img(vis=obj+'/'+obs, dirty_image=True)
+        except:
+            print("Error in imaging {}".format(obj))
         basename = os.path.basename(obs)
         myimagename = basename + '.cont.auto'
         exportfits(imagename=myimagename+'.image', fitsimage=myimagename+'.fits')
@@ -8,5 +17,5 @@ def gen_image(obj):
 
 
 if __name__ == '__main__':
-    obj = ''
+    obj = '../1924-2914'
     gen_image(obj)
