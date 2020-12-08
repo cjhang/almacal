01b5ebfc5ed0c9859cb2a6965a8d40d80f9ac2c5
Jianhang Chen <cjhastro@gmail.com>
2020-12-08 17:55:42 +0100

    add initial file for load all the help functions for almacal

---
+++ code/almacal_init.py
@@ -0,0 +1,7 @@
+import os
+
+home = os.path.expanduser('~')
+
+execfile(home+'/Documents/projects/almacal/code/imaging_utils.py')
+execfile(home+'/Documents/projects/almacal/code/readms.py')
+execfile(home+'/Documents/projects/almacal/code/almacal_run.py')
