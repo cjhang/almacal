21b1baa9c01c195d7ccf59209ebdba01a9f99e12
cjhang <cjhastro@gmail.com>
2021-03-01 00:47:30 +0100

    add plot_completeness

--- code/snippets/plot_completeness.py
+++ code/snippets/plot_completeness.py
@@ -2,15 +2,16 @@
 import json
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import CubicSpline
 
 J1303_5540_B7_daostarfinder = '../../data/simulation/J1303-5540_B7_threshold5_daostarfinder.json'
-J1303_5540_B7_peakfinder = '../../../data/simulation/J1303-5540_B7_threshold5_peakfind.json'
-J1933_6942_B7_daostarfinder = '../../../data/simulation/J1933-6942_B7_threshold5_daostarfinder.json'
-J1933_6942_B7_peakfinder = '../../../data/simulation/J1933-6942_B7_threshold5_findpeak.json'
-J1933_6942_B6_daostarfinder = '../../../data/simulation/J1933-6942_B7_threshold5_daostarfinder.json'
-J1933_6942_B6_peakfinder = '../../../data/simulation/J1933-6942_B7_threshold5_findpeak.json'
+J1303_5540_B7_peakfinder = '../../data/simulation/J1303-5540_B7_threshold5_peakfind.json'
+J1933_6942_B7_daostarfinder = '../../data/simulation/J1933-6942_B7_threshold5_daostarfinder.json'
+J1933_6942_B7_peakfinder = '../../data/simulation/J1933-6942_B7_threshold5_findpeak.json'
+J1933_6942_B6_daostarfinder = '../../data/simulation/J1933-6942_B6_threshold5_daostarfinder.json'
+J1933_6942_B6_peakfinder = '../../data/simulation/J1933-6942_B6_threshold5_findpeak.json'
 
-def plot_completeness(jsonfile, snr=np.arange(1.0, 10, 0.1), ax=None):
+def plot_completeness(jsonfile, snr=np.arange(1.0, 10, 0.2), ax=None):
     """
     """
     
@@ -46,18 +47,23 @@ def plot_completeness(jsonfile, snr=np.arange(1.0, 10, 0.1), ax=None):
         n_fake = n_found2 - np.sum(np.array(detection_found_array[:,1][snr_select2]))
         fake_rate_list.append(1.0*n_fake/n_found2)
 
-    print(completeness_list)
-    if ax is not None:
+    completeness_list = np.ma.masked_invalid(completeness_list)
+    snr_mid = 0.5*(snr[1:]+snr[:-1])
+    cs = CubicSpline(snr_mid, completeness_list.filled(0.))
+
+
+    if ax is None:
         fig, ax = plt.subplots(1,3, figsize=(12, 3))
     
     ax0 = ax[0]
     ax0.set_xlabel('SNR')
     ax0.set_ylabel(r'$S_{\rm out}/S_{\rm in}$')
-    ax0.plot(snr_input, flux_input_aperture/flux_input, 'k.', label='aperture')
-    ax0.plot(snr_input, flux_input_gaussian/flux_input, 'r.', label='gaussian')
+    ax0.plot(snr_input, flux_input_aperture/flux_input, 'k.', label='aperture', alpha=0.5)
+    ax0.plot(snr_input, flux_input_gaussian/flux_input, 'r.', label='gaussian', alpha=0.5)
     
     ax1 = ax[1]
-    ax1.plot(0.5*(snr[1:]+snr[:-1]), completeness_list, 'o')
+    ax1.plot(snr_mid, completeness_list, 'o')
+    ax1.plot(snr_mid, cs(snr_mid), '--')
     ax1.set_xlabel('SNR')
     ax1.set_ylabel(r'Completeness')
     ax1.set_ylim((-0.1, 1.2))
@@ -69,9 +75,11 @@ def plot_completeness(jsonfile, snr=np.arange(1.0, 10, 0.1), ax=None):
     ax2.set_xlim((0., 8))
     ax2.set_ylim((-0.1, 1.2))
 
-    # plt.show()
-
 if __name__ == '__main__':
     fig, ax = plt.subplots(1,3, figsize=(12,3))
+    # plot_completeness(J1303_5540_B7_peakfinder,ax=ax)
+    # plot_completeness(J1933_6942_B6_peakfinder,ax=ax)
     plot_completeness(J1303_5540_B7_daostarfinder,ax=ax)
+    plot_completeness(J1933_6942_B6_daostarfinder,ax=ax)
+    plot_completeness(J1933_6942_B7_daostarfinder,ax=ax)
     plt.show()
